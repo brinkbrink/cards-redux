b6657983aaffe142bf47de61fb9c3bcfcb57f478
E. Brink <brinkbrink@gmail.com>
2022-03-22 10:27:10 -0700

    init functionality

--- js/cards.js
+++ js/cards.js
@@ -2,3 +2,9 @@
 $( function() {
     $( "#tabs" ).tabs();
   } );
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
