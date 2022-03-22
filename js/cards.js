1c2297033e0ec7354fdc5aca400dfc19769d4944
E. Brink <brinkbrink@gmail.com>
2022-03-22 10:58:17 -0700

    init random functionality

--- js/cards.js
+++ js/cards.js
@@ -8,3 +8,10 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
   }
+
+document.getElementById("card1").innerHTML = getRandomInt(1,10);
+
+function assignStatement(int) {
+    int = getRandomInt(0,20)
+    
+}
