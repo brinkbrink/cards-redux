78448dedef361137d28138e5a6aa3a2edcb55a19
E. Brink <brinkbrink@gmail.com>
2022-03-23 10:41:06 -0700

    assigning random to card strings

--- js/cards.js
+++ js/cards.js
@@ -1,23 +1,181 @@
-// tabs function
+// jQuery tabs function
 $( function() {
     $( "#tabs" ).tabs();
   } );
 
+// producing random integers
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
   }
 
-document.getElementById("card1").innerHTML = getRandomInt(1,10);
+//test
+document.getElementById("cardp1").innerHTML = getRandomInt(1,10);
 
 // function assignStatement(int) {
 //     int = getRandomInt(0,20)
 // }
 
-let cards = ['Apple', 'ddd','dklj'];
 
-let x = getRandomInt(0,2);
-let y = cards[x];
 
-document.getElementById("card2").innerHTML = y;
+let cardsm = new Array();
+
+cardsm[0] = 'Switching to Geico.';
+cardsm[1] = 'Peeing a little bit.';
+cardsm[2] = 'The Jews.';
+cardsm[3] = 'Whatever.';
+cardsm[4] = 'Powerful thighs.';
+cardsm[5] = 'These hoes.';
+cardsm[6] = 'The only gay person in a hundred miles.';
+cardsm[7] = 'Donald J. Trump';
+cardsm[8] = 'Kissing grandma on the forehead and turning off her life support.';
+cardsm[9] = 'A certain je ne sais quoi.';
+cardsm[10] = 'An AR-15 assault rifle.';
+cardsm[11] = 'My good bra.';
+cardsm[12] = 'Punching a congressman in the face.';
+cardsm[13] = 'Saying everything is okay when everything is clearly not okay';
+cardsm[14] = 'Being rich.';
+cardsm[15] = 'Floating down the Hudson River with the other garbage.';
+cardsm[16] = 'Republicans.';
+cardsm[17] = 'Sniffing and kissing my feet.';
+cardsm[18] = 'A much younger woman';
+cardsm[19] = 'Poverty.';
+cardsm[20] = 'Kamala Harris.';
+cardsm[21] = 'Committing suicide.';
+cardsm[22] = 'A loser like you.';
+cardsm[23] = 'A mistake.';
+cardsm[24] = 'Wizard music.';
+cardsm[25] = 'The Kool-Aid Man.';
+cardsm[26] = 'Explaining the difference between sex and gender.';
+cardsm[27] = 'Free samples.';
+cardsm[28] = 'Feeding strawberries to my manslut.';
+cardsm[29] = 'The Three-Fifths Compromise.';
+cardsm[30] = 'Licking things to claim them as your own.'; 
+cardsm[31] = 'Jobs.';
+cardsm[32] = 'The placenta.';
+cardsm[33] = 'The Bachelorette season finale.';
+cardsm[34] = 'Throwing grapes at a man until he loses touch with reality.';
+cardsm[35] = 'Establishing dominance.';
+cardsm[36] = 'Finger painting.';
+cardsm[37] = 'Old-people smell.';
+cardsm[38] = 'Getting crushed by a vending machine.';
+cardsm[39] = 'My inner demons.';
+cardsm[40] = 'A Super Soaker™ full of cat pee.';
+cardsm[41] = "The feeling you get when the mosquitos arrive at dusk.";
+cardsm[42] = 'Cuddling.';
+cardsm[43] = 'However much weed $20 can buy.';
+cardsm[44] = 'Battlefield amputations.';
+cardsm[45] = 'Spaghetti? Again?';
+cardsm[46] = 'Ronald Reagan.';
+cardsm[47] = 'A disappointing birthday party.';
+cardsm[48] = 'Nachos for the table.';
+cardsm[49] = 'Becoming a blueberry.';
+cardsm[50] = 'A tiny horse.';
+cardsm[51] = 'Paying $20 for a beer at a baseball game.';
+cardsm[52] = 'A pangender octopus who roams the cosmos in search of love.';
+cardsm[53] = 'Meth.';
+cardsm[54] = 'Cyanide.';
+cardsm[55] = 'Holding down a child and farting all over him.';
+cardsm[56] = 'A Bop It.™';
+cardsm[57] = 'A whole thing of butter.';
+cardsm[58] = 'Still being a virgin.';
+cardsm[59] = 'Solving problems with violence.';
+cardsm[60] = 'A lifetime of sadness.';
+cardsm[61] = 'Two Xanax and a bottle of wine.';
+cardsm[62] = 'Bitches.';
+cardsm[63] = 'Fiery poops.';
+cardsm[64] = 'Saying “I love you.”';
+cardsm[65] = 'The true meaning of Christmas.';
+cardsm[66] = 'Whatever is in the fridge.';
+cardsm[67] = 'Owning and operating a Chilis franchise.';
+cardsm[68] = 'Estrogen.';
+cardsm[69] = 'Girls.';
+cardsm[70] = 'The Russians.';
+cardsm[71] = 'American Idol judge, Randy Jackson.';
+cardsm[72] = 'The American Dream.';
+cardsm[73] = 'Not wearing pants.';
+cardsm[74] = 'Pooping in a laptop and closing it.';
+cardsm[75] = 'A saxophone solo.';
+cardsm[76] = 'Italians.';
+cardsm[77] = 'A fetus.';
+cardsm[78] = 'Firing a rifle into the air while balls deep in a squealing hog.';
+cardsm[80] = 'Mike Pence.';
+cardsm[81] = 'What that mouth do.';
+cardsm[82] = 'Object permanence.';
+cardsm[83] = 'Opposable thumbs.';
+cardsm[84] = 'Racially-biased SAT questions.';
+cardsm[85] = 'The Great Depression.';
+cardsm[86] = 'Chainsaws for hands.';
+cardsm[87] = 'Nicolas Cage.';
+cardsm[88] = 'Like, whatever.';
+cardsm[89] = 'Explosions.';
+cardsm[90] = 'Not vaccinating my children because I am stupid.';
+cardsm[91] = 'Liberals.';
+cardsm[92] = 'Committing treason.';
+cardsm[93] = 'A horde of Vikings.';
+cardsm[94] = 'Vehicular manslaughter.';
+cardsm[95] = 'Women’s suffrage.';
+cardsm[96] = 'Some guy.';
+cardsm[97] = 'Judge Judy';
+cardsm[98] = 'A micropig wearing a tiny raincoat and booties.';
+cardsm[99] = 'A gambling problem.';
+cardsm[100] = 'Man meat.';
+cardsm[101] = 'Santa.';
+cardsm[102] = 'A martian';
+cardsm[103] = 'Tigre.';
+cardsm[104] = 'The wifi password.';
+cardsm[105] = 'Sugar cookies.';
+cardsm[106] = 'Being able to talk to elephants.';
+cardsm[107] = 'An icy stare.';
+cardsm[108] = 'Christmas tree vomit on the rug. Again.';
+cardsm[109] = 'A Christmas Story.';
+cardsm[110] = 'Unspeakable terrors.';
+cardsm[111] = 'Brothers.';
+cardsm[112] = 'Coffee.';
+cardsm[113] = 'Creatively wrapped gifts.';
+cardsm[114] = "Maintaining a 4.0 GPA.";
+cardsm[115] = 'Delicious mushrooms.';
+cardsm[116] = 'Gin and tonic.';
+cardsm[117] = 'Daffodils.';
+cardsm[118] = 'Smoking legal weed in Colorado.';
+cardsm[119] = 'Clementine segments.';
+cardsm[120] = 'Lavendar growing on the Olympic Peninsula.';
+
+// let cards = [
+// 'Apple', 
+// 'ddd',
+// 'dklj'
+// ];
+
+let s = getRandomInt(0,14);
+let t = getRandomInt(15,29);
+let u = getRandomInt(30,44);
+let v = getRandomInt(45,59);
+let w = getRandomInt(60,74);
+let x = getRandomInt(75,89);
+let y = getRandomInt(90,104);
+let z = getRandomInt(105,120);
+
+// add if statements to disallow repeat cards
+
+//add loop for each assignment below
+// let a = cards[x];
+let m1 = cardsm[s];
+let m2 = cardsm[t];
+let m3 = cardsm[u];
+let m4 = cardsm[v];
+let m5 = cardsm[w];
+let m6 = cardsm[x];
+let m7 = cardsm[y];
+let m8 = cardsm[z];
+
+// add loop for each hand
+document.getElementById("cardm1").innerHTML = m1;
+document.getElementById("cardm2").innerHTML = m2;
+document.getElementById("cardm3").innerHTML = m3;
+document.getElementById("cardm4").innerHTML = m4;
+document.getElementById("cardm5").innerHTML = m5;
+document.getElementById("cardm6").innerHTML = m6;
+document.getElementById("cardm7").innerHTML = m7;
+document.getElementById("cardm8").innerHTML = m8;
